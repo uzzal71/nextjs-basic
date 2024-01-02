c309ac59643cee7a462673050fdc7686c4454922
uzzal71 <uzzalroy.acm@gmail.com>
2024-01-02 23:47:58 +0600

    simple header

--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-      <nav>Home|About</nav>
+      <nav>Home|About|Contact</nav>
       {children}
       </body>
     </html>
