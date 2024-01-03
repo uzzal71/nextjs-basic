c473939da4ad65fad538f609eb4299dfc06e8342
uzzal71 <uzzalroy.acm@gmail.com>
2024-01-03 23:27:09 +0600

    page brack

--- app/layout.js
+++ app/layout.js
@@ -12,6 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+      <nav>Home | About</nav>
       {children}
       </body>
     </html>
--- app/page.js
+++ app/page.js
@@ -1,8 +1,10 @@
 
 export default function Home() {
   return (
-    <main>
-      <div>Hello world</div>
+    <main className="flex min-h-screen">
+      <div>
+        Id voluptate in ipsum laboris amet do esse excepteur dolor magna et. Est et aliquip fugiat est occaecat enim. Amet officia sit eiusmod ut do mollit qui ut mollit nostrud deserunt excepteur veniam ad.
+      </div>
     </main>
   )
 }
