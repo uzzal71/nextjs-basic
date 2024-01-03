139c41da8afc85a40c317083683b979799ca0212
uzzal71 <uzzalroy.acm@gmail.com>
2024-01-03 23:58:29 +0600

    page brack

---
+++ app/about/mission/page.js
@@ -0,0 +1,10 @@
+
+export default function Mission() {
+    return (
+        <main className="mt-10">
+            <div>
+            This is mission page
+        </div>
+        </main>
+    );
+}
