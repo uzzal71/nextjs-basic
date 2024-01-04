427a15c4bfa7d0745785fea3152a05a2d8ca65d4
uzzal71 <uzzalroy.acm@gmail.com>
2024-01-04 23:21:28 +0600

    page brack

---
+++ app/contact/page.js
@@ -0,0 +1,10 @@
+
+export default function Contact() {
+    return (
+        <main className="mt-10">
+            <div>
+            This is contact page
+        </div>
+        </main>
+    );
+}
